4a8adb49050a1daf4d94c425ade6845201a016d2
nimaMinooei <nima.minooei84@gmail.com>
2024-12-02 17:01:23 +0330

    update front

--- frontend/src/pages/Header2.js
+++ frontend/src/pages/Header2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { FaClock } from "react-icons/fa";
@@ -11,6 +11,7 @@ function Header2() {
   const [showHeader, setShowHeader] = useState(true);
   const [cards, setCards] = useState([]);
   const [time, setTime] = useState(""); // State for storing time
+  const navRef = useRef(null); // Ref for navigation element
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -55,16 +56,30 @@ function Header2() {
   const handleCategoryClick = (index) => {
     setCurrentCategoryIndex(index);
     fetchCards(categories[index].id);
+    scrollToCenter(index);
+  };
+
+  const scrollToCenter = (index) => {
+    const nav = navRef.current;
+    const item = nav.children[index];
+    const itemLeft = item.offsetLeft;
+    const itemWidth = item.clientWidth;
+    const navWidth = nav.clientWidth;
+    const scrollPosition = itemLeft - navWidth / 2 + itemWidth / 2;
+    nav.scroll({
+      left: scrollPosition,
+      behavior: "smooth",
+    });
   };
 
   return (
     <>
       {showHeader ? (
         <>
-          <header className="flex items-center justify-center rounded-3xl p-2 shadow-lg gap-[1em] italiana-regular ">
+          <header className="flex items-center justify-center rounded-3xl p-2 shadow-lg gap-[1em] italiana-regular">
             <button
               onClick={() => navigate("/")}
-              className="flex items-center justify-center text-black border-2  rounded-full w-10 h-10 transform transition-transform duration-300 ease-in-out hover:scale-110  "
+              className="flex items-center justify-center text-black rounded-full w-10 h-10 transform transition-transform duration-300 ease-in-out hover:scale-110"
             >
               <img
                 src="https://img.icons8.com/ios-glyphs/30/ffffff/back.png"
@@ -72,23 +87,27 @@ function Header2() {
                 className="w-7 h-7 mx-[2em] bg-blue-700 py-1 px-1 rounded-full"
               />
             </button>
-            <nav className="categories-nav flex justify-center items-center overflow-x-auto w-full whitespace-nowrap">
+            <nav className="categories-nav" ref={navRef}>
               {categories.length > 0 &&
                 categories.map((category, index) => (
                   <div
                     key={category.id}
-                    className={`flex flex-col items-center justify-center text-[.5em] text-white border-2 border-white rounded-full w-16 h-16 mx-2 transform transition-transform duration-300 ease-in-out text-center p-1 ${
-                      currentCategoryIndex === index ? "bg-blue-700" : ""
-                    }`}
-                    style={{ flex: "0 0 auto" }}
+                    className="category-item"
                     onClick={() => handleCategoryClick(index)}
                   >
-                    <img
-                      src={`/icons/${category.icon}`}
-                      alt={`${category.title} icon`}
-                      className="w-4 h-4 mb-1"
-                    />
-                    <span className="">{category.title}</span>
+                    <div
+                      className={`category-icon ${
+                        currentCategoryIndex === index
+                          ? "selected"
+                          : "unselected"
+                      }`}
+                    >
+                      <img
+                        src={`/icons/${category.icon}`}
+                        alt={`${category.title} icon`}
+                      />
+                    </div>
+                    <span className="text-xs">{category.title}</span>
                   </div>
                 ))}
             </nav>
@@ -97,36 +116,35 @@ function Header2() {
               {/* Added margin to separate logo and categories */}
               <img
                 onClick={() => navigate("/")}
-                className="h-[3em] w-[3em]  mx-[2em] bg-[#0E48C5] rounded-full"
+                className="h-[3em] w-[3em] mx-[2em] bg-[#0E48C5] rounded-full"
                 src="/images/logohome.png"
                 alt="Logo"
               />
             </div>
           </header>
-          <div className="container flex flex-col justify-center items-center gap-4 p-4 italiana-regular   ">
+          <div className="container flex flex-col justify-center items-center gap-4 p-4 italiana-regular">
             {cards.map((card) => (
               <div
                 key={card.id}
-                className="flex flex-col justify-center items-center  text-white border-[.3em] border-[#0E48C5] rounded-3xl shadow-xl w-80 p-[.3em] m-4 bg-green-200 bg-opacity-30"
+                className="flex flex-col justify-center items-center text-white border-[.3em] border-[#0E48C5] rounded-3xl shadow-xl w-80 p-[.3em] m-4 bg-green-200 bg-opacity-30"
               >
                 <img
                   src={card.image}
                   alt={card.title}
                   className="card-image rounded-lg mb-4 object-cover w-[80%] h-[15em]"
                 />
-
-                <h2 className="flex justify-center items-center text-2xl py-1 px-1 font-bold bg-blue-700 rounded-full w-full   ">
+                <h2 className="flex justify-center items-center text-2xl py-1 px-1 font-bold bg-blue-700 rounded-full w-full">
                   {card.title}
                 </h2>
                 <p className="flex justify-centr items-start text-gray-600">
                   {card.description}
                 </p>
                 <div className="flex justify-between gap-[12em] items-center mt-4">
-                  <div className="text-lg font-semibold  bg-blue-700  rounded-full py-1 px-1  flex items-center">
+                  <div className="text-lg font-semibold bg-blue-700 rounded-full py-1 px-1 flex items-center">
                     T{card.price}
                   </div>
-                  <div className="text-lg font-semibold  bg-blue-700  rounded-full py-1 px-1  flex items-center">
-                    <FaClock className="ml-2" /> {time}10
+                  <div className="text-lg font-semibold bg-blue-700 rounded-full py-1 px-1 flex items-center">
+                    <FaClock className="ml-2" /> {time}
                   </div>
                 </div>
               </div>
