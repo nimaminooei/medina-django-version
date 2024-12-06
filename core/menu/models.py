cc67406d9940b88d519af2ab8dccafc1fd45c11d
nimaMinooei <nima.minooei84@gmail.com>
2024-12-06 21:46:55 +0330

    d

--- core/menu/models.py
+++ core/menu/models.py
@@ -4,13 +4,12 @@ from adminsortable.models import SortableMixin
 class category(SortableMixin):
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
-    order = models.PositiveIntegerField(default=0, editable=False, db_index=True)  # فیلد ترتیب
-
+    order = models.PositiveIntegerField(default=0, editable=False, db_index=True)  
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ['order']  # مرتب‌سازی بر اساس ترتیب
+        ordering = ['order']  
 
 class item(SortableMixin):
     category = models.ForeignKey(category, on_delete=models.SET_NULL, null=True, blank=False)
@@ -19,7 +18,7 @@ class item(SortableMixin):
     time = models.IntegerField(null=True)
     description = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
-    order = models.PositiveIntegerField(default=0, editable=False, db_index=True)  # فیلد ترتیب
+    order = models.PositiveIntegerField(default=0, editable=False, db_index=True)  
 
     def __str__(self):
         return self.name
